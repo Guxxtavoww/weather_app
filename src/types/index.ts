85c6484b9f883983d30d7b7d6bd9f0924804a85a
Gustavo <gustavoaugustocar@hotmail.com>
2023-02-21 15:15:13 -0300

    adding development features

---
+++ src/types/index.ts
@@ -0,0 +1,18 @@
+import { FC, ReactNode } from 'react';
+
+export type Children = {
+  children: ReactNode;
+};
+
+export type FCWithChildren<
+  ComponentProps extends object = {},
+  IsChildrenRequired extends boolean = false,
+> = FC<
+  IsChildrenRequired extends true
+    ? ComponentProps & Children
+    : ComponentProps & Partial<Children>
+>;
+
+export type LooseAutoComplete<T extends string | number> =
+  | T
+  | Omit<string | number, T>;
